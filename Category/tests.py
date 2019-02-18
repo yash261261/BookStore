f8a9da8957e1c8c00ae6c53d2a7a750f8d4fe43f
yash261261 <yash261261@gmail.com>
2019-02-18 15:21:15 -0800

    Tests added

--- Category/tests.py
+++ Category/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
-
+import unittest
 # Create your tests here.
+
+class TestSum(unittest.TestCase):
+
+    def test_sum(self):
+        self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
+
+    def test_sum_tuple(self):
+        self.assertEqual(sum((1, 2, 2)), 6, "Should be 6")
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
+# class evenTestCase(unittest.TestCase):
+#
+#     def __init__(self,number):
+#         self.number = number
+#
+#     def Enum(self):
+#         if self.number%2==0:
+#             return True
+#         else:
+#             return False
+#
+#     def is_Enum(self):
+#         self.assertTrue()
+#
+#
+#
+# if __name__ == '__main__':
+#     unittest.main()
